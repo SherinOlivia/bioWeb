27d8115aa21966a8fc0079023b310acb8894ad98
Sherin Olivia <sochronicle@gmail.com>
2024-03-01 16:30:48 +0700

    feat: added the about me section to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
-
+import AboutSection from "./components/AboutSection";
 
 export default function Home() {
-  return  <main className="flex min-h-screen flex-col bg-[#121212] container ">
-    <div className="container mt-24 mx-auto px-12 py-4">
+  return  (
+    <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
-      <HeroSection />
-    </div>
-  </main>
-  
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        <br></br>
+        <AboutSection />
+      </div>
+    </main>
+  )
 }
